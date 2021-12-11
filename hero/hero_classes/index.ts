8865bcce0c46e971aa248d9066ea179e500458e8
adolecumberri <adolecumberri@gmail.com>
2021-12-11 22:53:13 +0100

    added classes

---
+++ hero/hero_classes/index.ts
@@ -0,0 +1,14 @@
+import { Archer } from "./archer";
+import { Berserker } from "./berserker";
+import { Defender } from "./defender";
+import { Fencer } from "./fencer";
+import { Ninja } from "./ninja";
+import { Paladin } from "./paladin";
+import { Sniper } from "./sniper";
+import { Soldier } from "./soldier";
+import { Thieve } from "./thieve";
+
+type IHero = Archer | Berserker | Defender | Fencer | Ninja | Paladin | Sniper | Soldier;
+export type { IHero };
+export { Archer, Berserker, Defender, Fencer, Ninja, Paladin, Sniper, Soldier, Thieve };
+
