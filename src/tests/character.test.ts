e781df50e8ec287d4e33e5936bfb31d1e55e9366
adolecumberri <adolecumberri@gmail.com>
2022-06-12 16:14:34 +0200

    WIP: testing basic character checks

--- src/tests/character.test.ts
+++ src/tests/character.test.ts
@@ -1,6 +1,84 @@
 import Character from '../classes/Character'
 
+jest.mock('../classes/Character') // Character is now a mock constructor.
 
 describe('Character tests', () => {
-    let char = new Character()
+
+    const stats = {
+        correct_basic_stats: {
+            accuracy: 1,
+            attack: 1,
+            att_interval: 1,
+            att_speed: 1,
+            crit: 0,
+            critDamage: 1,
+            currentHp: 0,
+            deffence: 0,
+            evasion: 0,
+            hp: 0,
+        },
+        incorrect_high_stats: {
+            accuracy: -11,
+            attack: -43,
+            att_interval: -23,
+            att_speed: 0,
+            crit: -1,
+            critDamage: 0,
+            currentHp: -9,
+            deffence: 0,
+            evasion: -9,
+            hp: -4,
+        }
+    }
+
+    const expected_basic_stats = {
+        "accuracy": 1,
+        "att_interval": 1,
+        "att_speed": 1,
+        "attack": 1,
+        "crit": 0,
+        "critDamage": 1,
+        "currentHp": 0,
+        "deffence": 0,
+        "evasion": 0,
+        "hp": 0
+    }
+
+    const expected_basic_object = {
+        "actions": undefined,
+        "alive": true,
+        "checkErrors": jest.fn(),
+        "getProb": jest.fn(),
+        "rand": jest.fn(),
+        "stats": expected_basic_stats
+    }
+    
+    test('Character Constructor is called', ()=> {
+        let char = new Character()
+        expect(Character).toHaveBeenCalledTimes(1);
+    })
+
+    test('Created Character without params', () => {
+        let char = new Character()
+
+        expect(char).toEqual(expected_basic_object)
+    })
+
+    test('Created Character with correct Stats', () => {
+        let char = new Character({
+            stats: stats.correct_basic_stats
+        })
+
+        expect(char).toEqual(Character)
+    })
+
+    test('Created Character with wrong Stats', () => {
+        let char = new Character({
+            stats: stats.incorrect_high_stats
+        })
+
+        //TODO: when error bransh will be pushed, this will work.
+        expect(() => char).toThrowError(Error)
+    })
+
 })
